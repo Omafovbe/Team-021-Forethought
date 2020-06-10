4d69e55677ceeef837b15aa30ac735483581c415
Adeyossy <adeyossy1@gmail.com>
2020-06-10 03:25:57 +0100

    fix: auth form should work flawlessly

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,16 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-// const path = require('path');
-
-// router.set('views', path.join(__dirname, 'views'));
-// router.set('view engine', 'ejs');
-
-// router.use(express.static(path.join(__dirname, 'static')));
-
-// router.use(express.json());
-// router.use(express.urlencoded( { extended: true }));
-
 router.get('/users/auth', (req, res) => {
     // console.log(req.query.isNew);
     
