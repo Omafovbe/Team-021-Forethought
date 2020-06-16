53d8b5cd7565e3f6825c3c4b91dfe11a7defdcbb
Adeyossy <adeyossy1@gmail.com>
2020-06-16 21:53:06 +0100

    added the consultant page

--- src/routes/consultant_connect.js
+++ src/routes/consultant_connect.js
@@ -1,5 +1,9 @@
 const router = require('express').Router();
 
+router.get('/consultants/profile', (req, res) => {
+  res.render('consultant');
+})
+
 // replace route with /users/:userid/connect
 router.get('/users/connect', (req, res) => {
   res.render('consultant_connect');
